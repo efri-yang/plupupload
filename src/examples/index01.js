492318bb282b1c94b42ea8fc9c2592c88a50844d
efri-yang <948061564@qq.com>
2018-05-03 08:39:10 +0800

    c

--- src/examples/index01.js
+++ src/examples/index01.js
@@ -1,233 +1,235 @@
+var uploader = new plupload.Uploader({
+    runtimes: 'html5,flash,silverlight,html4',
+    browse_button: 'img-add-btn-1', // you can pass an id...
+    url: 'upload2.php',
+    flash_swf_url: '../js/Moxie.swf',
+    silverlight_xap_url: '../js/Moxie.xap',
+    filters: {
+        prevent_duplicates: true,
+        max_file_size: '1000mb',
+        mime_types: [
+            { title: "Image files", extensions: "jpg,gif,png,psd" },
+            { title: "Zip files", extensions: "zip" }
+        ]
+    }
+});
+
+
+var $uploadBtn = $(".img-upload-btn");
+var $addBtn = $("#img-add-btn-1");
+var $insertBtn = $(".ft-btn-insert");
+var $fileListUL = $(".ckeditor-uploadfile-list");
+
+
+
+uploader.bind("Init", function(uploader) {
+    console.group("Init事件:当Plupload初始化完成后触发监听函数参数：(uploader)");
+});
+
+uploader.bind("PostInit", function() {
+    console.group("PostInit事件:当Init事件发生后触发监听函数参数：(uploader)");
+    $(".img-upload-btn").on("click", function() {
+        uploader.start();
+    });
+});
+
+uploader.bind("Browse", function(up) {
+    console.group("Browse事件")
+});
+
+
+
+uploader.bind('FileFiltered', function(up, file) {
+    console.group("FileFiltered事件")
+});
 
 
 
 
-    
-   var uploader = new plupload.Uploader({
-       runtimes: 'html5,flash,silverlight,html4',
-       browse_button: 'img-add-btn-1', // you can pass an id...
-       url: 'https://wnworld.com/api/plupload/upload.php',
-       flash_swf_url: '../js/Moxie.swf',
-       silverlight_xap_url: '../js/Moxie.xap',
-       filters: {
-           prevent_duplicates: true,
-           max_file_size: '10mb',
-           mime_types: [
-               { title: "Image files", extensions: "jpg,gif,png,psd" },
-               { title: "Zip files", extensions: "zip" }
-           ]
-       }
-   });
+function preloadThumb(file, cb) {
+    var img = new moxie.image.Image();
+    var resolveUrl = moxie.core.utils.Url.resolveUrl;
+    img.onload = function() {
+        var thumb = $('#' + file.id);
+        thumb.find(".img-before-preview").hide();
+        this.embed(thumb[0], {
+            // width: self.options.thumb_width,
+            // height: self.options.thumb_height,
+            width: 110,
+            height: 110,
+            crop: true,
+            fit: true,
+            preserveHeaders: false,
+            swf_url: resolveUrl(uploader.settings.flash_swf_url),
+            xap_url: resolveUrl(uploader.settings.silverlight_xap_url)
+        });
+    };
 
+    img.bind("embedded error", function(e) {
+        //不支持的浏览器就会触发error事件,支持的浏览器会触发embedded  e.type
+        this.destroy();
+        setTimeout(function() {
+            cb.call(null, e.type)
+        }, 1); // detach, otherwise ui might hang (in SilverLight for example)
+    });
 
-   var $uploadBtn=$(".img-upload-btn");
-   var $addBtn=$("#img-add-btn-1");
-   var $insertBtn=$(".ft-btn-insert");
-   var $fileListUL=$(".wangeditor-uploadfile-ft");
-
-
-
-   uploader.bind("Init", function(uploader) {
-       console.group("Init事件:当Plupload初始化完成后触发监听函数参数：(uploader)");
-   });
-
-   uploader.bind("PostInit", function() {
-       console.group("PostInit事件:当Init事件发生后触发监听函数参数：(uploader)");
-       $(".img-upload-btn").on("click", function() {
-           uploader.start();
-       });
-   });
-
-   uploader.bind("Browse", function(up) {
-       console.group("Browse事件")
-   });
-
-
-
-   uploader.bind('FileFiltered', function(up, file) {
-       console.group("FileFiltered事件")
-   });
+    $('#' + file.id).removeClass('plupload_thumb_toload').addClass('plupload_thumb_loading');
+    img.load(file.getSource());
+}
 
 
 
 
-   function preloadThumb(file, cb) {
-       var img = new moxie.image.Image();
-       var resolveUrl = moxie.core.utils.Url.resolveUrl;
-       img.onload = function() {
-           var thumb = $('#' + file.id);
-           thumb.find(".img-before-preview").hide();
-           this.embed(thumb[0], {
-               // width: self.options.thumb_width,
-               // height: self.options.thumb_height,
-               width: 110,
-               height: 110,
-               crop: true,
-               fit: true,
-               preserveHeaders: false,
-               swf_url: resolveUrl(uploader.settings.flash_swf_url),
-               xap_url: resolveUrl(uploader.settings.silverlight_xap_url)
-           });
-       };
+uploader.bind('FilesAdded', function(up, files) {
+    console.group("FilesAdded事件");
+    console.dir(files);
+    $(".img-upload-btn").removeClass("disabled");
 
-       img.bind("embedded error", function(e) {
-           //不支持的浏览器就会触发error事件,支持的浏览器会触发embedded  e.type
-           this.destroy();
-           setTimeout(function() {
-               cb.call(null, e.type)
-           }, 1); // detach, otherwise ui might hang (in SilverLight for example)
-       });
+    var str = "";
+    var $beforePreview;
+    var $afterPreview;
+    plupload.each(files, function(file) {
+        var str = '<li id="' + file.id + '">' +
+            '<div class="img-before-preview">' +
+            '<p class="title">' + file.name + '</p>' +
+            '<p class="txt-error-tip"></p>' +
+            '<p class="img-loading"></p>' +
+            '</div>' +
+            '<div class="img-after-preview">' +
+            '<img src="" class="img-upload" />' +
+            '</div>' +
+            '<div class="img-progress"><span></span></div>' +
+            '<a href="#" class="img-del-btn"></a>' +
+            '<span class="img-success"></span>' +
+            '<span class="img-error">上传失败，请重试</span>' +
+            '</li>';
+        var $str = $(str);
+        $str.appendTo($fileListUL);
+        $beforePreview = $str.find('.img-before-preview');
+        $afterPreview = $str.find(".img-after-preview");
+        $del = $str.find(".img-del-btn");
 
-       $('#' + file.id).removeClass('plupload_thumb_toload').addClass('plupload_thumb_loading');
-       img.load(file.getSource());
-   }
-
-
-
-
-   uploader.bind('FilesAdded', function(up, files) {
-       console.group("FilesAdded事件");
-       console.dir(files);
-       $(".img-upload-btn").removeClass("disabled");
-
-       var str = "";
-       var $beforePreview;
-       var $afterPreview;
-       plupload.each(files, function(file) {
-           var str = '<li id="' + file.id + '">' +
-               '<div class="img-before-preview">' +
-               '<p class="title">' + file.name + '</p>' +
-               '<p class="txt-error-tip"></p>' +
-               '<p class="img-loading"></p>' +
-               '</div>' +
-               '<div class="img-after-preview">' +
-               '<img src="" class="img-upload" />' +
-               '</div>' +
-               '<div class="img-progress"><span></span></div>' +
-               '<a href="#" class="img-del-btn"></a>' +
-               '<span class="img-success"></span>' +
-               '<span class="img-error">上传失败，请重试</span>' +
-               '</li>';
-           var $str = $(str);
-           $str.appendTo($('.wangeditor-uploadfile-list'));
-           $beforePreview = $str.find('.img-before-preview');
-           $afterPreview = $str.find(".img-after-preview");
-           $del=$str.find(".img-del-btn");
-
-           $del.show();
-           $del.on("click",function(){
-                uploader.removeFile(file);
-                console.dir(uploader.files);
-
-
-
-           })
+        $del.show();
+        $del.on("click", function() {
+            uploader.removeFile(file);
             console.dir(uploader.files);
-            console.dir(uploader.total);
-           preloadThumb(file, function(type) {
-                $beforePreview.hide();
-
-
-           })
-       });
-   });
 
 
 
+        })
+
+        preloadThumb(file, function(type) {
+           if(type=="error"){
+              $str.find(".img-loading").hide();
+              $str.find(".txt-error-tip").text("该浏览器不支持图片预览").show();
+
+           }else{
+            $beforePreview.hide();
+           }
+            
 
 
-   uploader.bind('QueueChanged', function(up) {
-       console.group("QueueChanged事件");
+        })
+    });
+});
 
-   });
 
-   uploader.bind('Refresh', function(up) {
-       console.group("Refresh事件");
-       if(!up.total.queued){
-            $insertBtn.addClass("disabled");
-       }else{
+
+
+
+uploader.bind('QueueChanged', function(up) {
+    console.group("QueueChanged事件");
+
+});
+
+uploader.bind('Refresh', function(up) {
+    console.group("Refresh事件");
+    if (!up.total.queued) {
+        $insertBtn.addClass("disabled");
+    } else {
         $insertBtn.removeClass("disabled");
-       }
-   });
+    }
+});
 
 
 
 
-   uploader.bind('BeforeUpload', function(up, file) {
-       console.group("BeforeUpload事件");
-       //
-       var $li = $("#" + file.id);
-       var $propress = $li.find(".img-progress").show();
-   });
-   uploader.bind('UploadProgress', function(up, file) {
-       console.group("UploadProgress事件");
-       console.dir(file);
-       var $li = $("#" + file.id);
-       var $propress = $li.find(".img-progress").show();
-       $propress.children('span').css("width", file.percent + "%");
-   });
+uploader.bind('BeforeUpload', function(up, file) {
+    console.group("BeforeUpload事件");
+    console.dir(file);
+    var $li = $("#" + file.id);
+    var $propress = $li.find(".img-progress").show();
+});
+uploader.bind('UploadProgress', function(up, file) {
+    console.group("UploadProgress事件");
+    console.dir(file);
+    var $li = $("#" + file.id);
+    var $propress = $li.find(".img-progress").show();
+    $propress.children('span').css("width", file.percent + "%");
+});
 
 
 
 
-   uploader.bind('FileUploaded', function(up, file, info) {
-       console.group("FileUploaded事件")
-   });
-
-   uploader.bind('StateChanged', function(up) {
-       //当前的上传状态，可能的值为plupload.STARTED或plupload.STOPPED，该值由Plupload实例的stop()或statr()方法控制。默认为plupload.STOPPED
-       console.group("StateChanged事件");
-       console.dir(up.state);
-   });
-
-   uploader.bind('UploadComplete', function(up, files) {
-       console.group("UploadComplete事件");
-
-   });
+uploader.bind('FileUploaded', function(up, file, info) {
+    console.dir(info);
+    console.group("FileUploaded事件");
+    var $li = $("#" + file.id);
+    var $propress = $li.find(".img-progress").hide();
+    $li.find(".img-success").show();
+    $li.attr("data-src", info.response)
 
 
+});
 
+uploader.bind('StateChanged', function(up) {
+    //当前的上传状态，可能的值为plupload.STARTED或plupload.STOPPED，该值由Plupload实例的stop()或statr()方法控制。默认为plupload.STOPPED
+    console.group("StateChanged事件");
+    console.dir(up.state);
+});
 
+uploader.bind('UploadComplete', function(up, files) {
+    console.group("UploadComplete事件");
 
-
-   uploader.bind('FilesRemoved', function(up, files) {
-       console.group("FilesRemoved事件");
-
-           $.each(files,function(index,file){
-                 
-                    $("#"+file.id).remove();
-                  
-           });
-          
-
-           setTimeout(function(){
-            alert(uploader.total.queued)
-                if(!uploader.total.queued){
-                 $uploadBtn.addClass("disabled");
-               }
-           })
-       
-
-
-   });
-
-   uploader.bind('ChunkUploaded', function(up, file, info) {
-       console.group("ChunkUploaded事件")
-   });
-
-   uploader.bind('Destroy', function() {
-       console.group("Destroy事件")
-   });
-
-   uploader.bind('OptionChanged', function(up, name, value, oldValue) {
-       console.group("OptionChanged事件")
-   });
+});
 
 
 
 
 
 
+uploader.bind('FilesRemoved', function(up, files) {
+    console.group("FilesRemoved事件");
+
+    $.each(files, function(index, file) {
+
+        $("#" + file.id).remove();
+
+    });
+
+
+    setTimeout(function() {
+        alert(uploader.total.queued)
+        if (!uploader.total.queued) {
+            $uploadBtn.addClass("disabled");
+        }
+    })
+
+
+
+});
+
+uploader.bind('ChunkUploaded', function(up, file, info) {
+    console.group("ChunkUploaded事件")
+});
+
+uploader.bind('Destroy', function() {
+    console.group("Destroy事件")
+});
+
+uploader.bind('OptionChanged', function(up, name, value, oldValue) {
+    console.group("OptionChanged事件")
+});
 
 
 
@@ -237,4 +239,10 @@
 
 
 
-   uploader.init();
+
+
+
+
+
+
+uploader.init();
--- src/examples/upload.php
+++ src/examples/upload.php
@@ -1,205 +1,26 @@
 <?php
-/**
- * upload.php
- *
- * Copyright 2013, Moxiecode Systems AB
- * Released under GPL License.
- *
- * License: http://www.plupload.com/license
- * Contributing: http://www.plupload.com/contributing
- */
+header("Access-Control-Allow-origin:*");
+require_once "PluploadHandler.php";
 
-#!! 注意
-#!! 此文件只是个示例，不要用于真正的产品之中。
-#!! 不保证代码安全性。
+$ph = new PluploadHandler(array(
+    'target_dir' => '../uploads/',
+    'allow_extensions' => 'jpg,jpeg,png',
+));
 
-#!! IMPORTANT:
-#!! this file is just an example, it doesn't incorporate any security checks and
-#!! is not recommended to be used in production environment as it is. Be sure to
-#!! revise it and customize to your needs.
+$ph->sendNoCacheHeaders();
+$ph->sendCORSHeaders();
 
-// Make sure file is not cached (as it happens for example on iOS devices)
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
-header("Access-Control-Allow-Headers: X-Requested-With"); 
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-header("Cache-Control: no-store, no-cache, must-revalidate");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-
-// Support CORS
-// header("Access-Control-Allow-Origin: *");
-// other CORS headers if any...
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    exit; // finish preflight CORS requests here
-}
-
-if (!empty($_REQUEST['debug'])) {
-    $random = rand(0, intval($_REQUEST['debug']));
-    if ($random === 0) {
-        header("HTTP/1.0 500 Internal Server Error");
-        exit;
-    }
-}
-
-// header("HTTP/1.0 500 Internal Server Error");
-// exit;
-
-// 5 minutes execution time
-@set_time_limit(5 * 60);
-// 设置脚本最大执行时间
-//
-//
-// Uncomment this one to fake upload time
-// usleep(5000);
-
-// Settings
-// $targetDir = ini_get("upload_tmp_dir") . DIRECTORY_SEPARATOR . "plupload";
-// 虚拟文件存放的目录
-$targetDir = 'upload_tmp';
-
-//文件上传后的目录
-$uploadDir = 'upload';
-
-$cleanupTargetDir = false; // Remove old files
-$maxFileAge = 5 * 3600; // Temp file age in seconds
-
-// Create target dir
-// 文件目录不存在 那么就创建一个
-if (!file_exists($targetDir)) {
-    @mkdir($targetDir);
-}
-
-// Create target dir
-if (!file_exists($uploadDir)) {
-    @mkdir($uploadDir);
-}
-
-// Get a file name
-if (isset($_REQUEST["name"])) {
-    $fileName = $_REQUEST["name"];
-} elseif (!empty($_FILES)) {
-    $fileName = $_FILES["file"]["name"];
+if ($result = $ph->handleUpload()) {
+    die(json_encode(array(
+        'OK' => 1,
+        'info' => $result,
+    )));
 } else {
-    $fileName = uniqid("file_");
+    die(json_encode(array(
+        'OK' => 0,
+        'error' => array(
+            'code' => $ph->getErrorCode(),
+            'message' => $ph->getErrorMessage(),
+        ),
+    )));
 }
-
-$oldName = $fileName;
-
-// $filePath = $targetDir . DIRECTORY_SEPARATOR . $fileName;
-// $uploadPath = $uploadDir . DIRECTORY_SEPARATOR . $fileName;
-
-
-$filePath = $targetDir ."/". $fileName;
-$uploadPath = $uploadDir ."/". $fileName;
-//DIRECTORY_SEPARATOR>\  就是反斜杠的意思
-// Chunking might be enabled
-// 获取变量的整数值
-$chunk = isset($_REQUEST["chunk"]) ? intval($_REQUEST["chunk"]) : 0;
-$chunks = isset($_REQUEST["chunks"]) ? intval($_REQUEST["chunks"]) : 1;
-
-// Remove old temp files
-//
-//  打开目录句柄 opendir
-if ($cleanupTargetDir) {
-    if (!is_dir($targetDir) || !$dir = opendir($targetDir)) {
-        die('{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Failed to open temp directory."}, "id" : "id"}');
-    }
-
-    while (($file = readdir($dir)) !== false) {
-        $tmpfilePath = $targetDir . DIRECTORY_SEPARATOR . $file;
-
-        // If temp file is current file proceed to the next
-        if ($tmpfilePath == "{$filePath}_{$chunk}.part" || $tmpfilePath == "{$filePath}_{$chunk}.parttmp") {
-            continue;
-        }
-
-        // Remove temp file if it is older than the max age and is not the current file
-        if (preg_match('/\.(part|parttmp)$/', $file) && (@filemtime($tmpfilePath) < time() - $maxFileAge)) {
-            @unlink($tmpfilePath);
-        }
-    }
-    closedir($dir);
-}
-
-// Open temp file
-if (!$out = @fopen("{$filePath}_{$chunk}.parttmp", "wb")) {
-    die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
-}
-
-if (!empty($_FILES)) {
-    if ($_FILES["file"]["error"] || !is_uploaded_file($_FILES["file"]["tmp_name"])) {
-        die('{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}');
-    }
-
-    // Read binary input stream and append it to temp file
-    // http://www.cnblogs.com/Romi/archive/2012/02/29/2374769.html
-    // 在区分二进制文件和文本文件的系统上（如 Windows）打开文件时，fopen() 函数的 mode 参数要加上 'b'。
-    if (!$in = @fopen($_FILES["file"]["tmp_name"], "rb")) {
-        die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
-    }
-} else {
-    if (!$in = @fopen("php://input", "rb")) {
-        die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
-    }
-}
-
-while ($buff = fread($in, 4096)) {
-    fwrite($out, $buff);
-}
-
-@fclose($out);
-@fclose($in);
-
-rename("{$filePath}_{$chunk}.parttmp", "{$filePath}_{$chunk}.part");
-
-$index = 0;
-$done = true;
-for ($index = 0; $index < $chunks; $index++) {
-    if (!file_exists("{$filePath}_{$index}.part")) {
-        $done = false;
-        break;
-    }
-}
-if ($done) {
-    $pathInfo = pathinfo($fileName);
-    if (!$out = @fopen($uploadPath, "wb")) {
-        die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
-    }
-    // flock — 轻便的咨询文件锁定   取得独占锁定（写入的程序。
-    if (flock($out, LOCK_EX)) {
-        for ($index = 0; $index < $chunks; $index++) {
-            if (!$in = @fopen("{$filePath}_{$index}.part", "rb")) {
-                break;
-            }
-
-            while ($buff = fread($in, 4096)) {
-                fwrite($out, $buff);
-            }
-
-            @fclose($in);
-            //删除文件01.jpg_0.part
-            // @unlink("{$filePath}_{$index}.part");
-        }
-
-        flock($out, LOCK_UN);
-    }
-    @fclose($out);
-
-    // $response = [
-    //     'success' => true,
-    //     'oldName' => $oldName,
-    //     'filePath' =>
-    //     'fileSize' => $_REQUEST['size'],
-    //     'fileSuffixes' => $pathInfo['extension'],
-    //     'file_id' => $_REQUEST['id'],
-    // ];
-   
-
-}
-
-
-// Return Success JSON-RPC response
- $filePath="http://localhost/plupupload/" . $uploadDir . "/" . $fileName;
-    die('{"jsonrpc" : "2.0", "result"'.$filePath.':, "id" : "id"}');
